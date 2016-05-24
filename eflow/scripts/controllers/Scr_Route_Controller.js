ca6c4fe744c9a08beb6b0a8808f753528039bbfb
Katherine Fallas <Katherinefallasc@gmail.com>
2016-05-24 17:21:28 +0000

    sdfg

--- eflow/scripts/controllers/Scr_Route_Controller.js
+++ eflow/scripts/controllers/Scr_Route_Controller.js
@@ -32,6 +32,7 @@ $scope.init = function(){
 				tilesloaded: function(e){					
 					$scope.Show_Components.Route_Form = false;	
 	                $scope.Show_Components.Route_Table = true;
+	                GMaps.off(,map);
                 }
 			});	
 		
