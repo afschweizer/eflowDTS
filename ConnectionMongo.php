ae57db26d2ea0b5dcacf55b2a20b536c3fbaa024
Katherine Fallas <Katherinefallasc@gmail.com>
2016-05-26 20:44:24 +0000

    Agregando nueva base de datos

--- ConnectionMongo.php
+++ ConnectionMongo.php
@@ -1,12 +1,22 @@
 <?php
 try{
-  
+/*  
 $uri = "mongodb://adminEprac:wolfe@ds053312.mongolab.com:53312/eflow"; 
 $options = array("connectTimeoutMS" => 30000);
 
 $conn = new MongoClient($uri,$options);
   
 $db = $conn->selectDB("eflow");
+  
+ */ /*------------------------------------------------------------*/
+  
+$uri = "mongodb://adminEprac:wolfe@ds015953.mlab.com:15953/eflow_testing"; 
+$options = array("connectTimeoutMS" => 30000);
+
+$conn = new MongoClient($uri,$options);
+  
+$db = $conn->selectDB("eflow");
+  
   
 }
 catch(MongoConnectionException $e) {
