0441f8132358f120384d9b7e3b96e1ee6e4c6a4b
Katherine Fallas <Katherinefallasc@gmail.com>
2016-06-02 19:47:08 +0000

    todo

--- Select_Vehicle.php
+++ Select_Vehicle.php
@@ -2,18 +2,19 @@
 
 include 'ConnectionMongo.php'; 
   
+
+
+
 $coll = $db->Store_Vehicle;
 
 $result = $coll->find($dataObject->Data);
 
-$arrayResult = Array();
-  
 foreach($result as $doc){
  
-  array_push($arrayResult, $doc);
-  
-}
+ array_push($arrayResult, $doc);
+ //$coll->update(array("_id"=> new MongoId($doc['_id'])), array('$set' => array("Transferring_State" => "Sync_With_Mobile")));
 
+}
 echo json_encode($arrayResult);
 
 ?>
