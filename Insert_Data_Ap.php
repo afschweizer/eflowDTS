df7e7a99f1266b85a4ad717abdafc18d6a2b55de
Katherine Fallas <Katherinefallasc@gmail.com>
2016-06-08 15:56:02 +0000

    cambios de sumary

--- Insert_Data_Ap.php
+++ Insert_Data_Ap.php
@@ -17,6 +17,21 @@
                   require 'Update_Jobs_Ap.php';
                   break;
                 } 
+              case 'Store_Trip':
+                {
+                  require 'Insert_Summary_Trip.php';
+                  break;
+                }  
+              case 'Store_VisitPoint':
+                {
+                  require 'Insert_Summary_VisitPoint.php';
+                  break;
+                } 
+              case 'Store_Item':
+                {
+                  require 'Insert_Summary_Item.php';
+                  break;
+                } 
               default:
                 {
                   require 'Insert_Default.php';
