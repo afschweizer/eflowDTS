c73630c4c40a97f9be29b635b53bfe99b30f2db9
Katherine Fallas <Katherinefallasc@gmail.com>
2016-06-17 22:55:55 +0000

    gh

--- Select_Summary_Visit_Point.php
+++ Select_Summary_Visit_Point.php
@@ -4,7 +4,7 @@ include 'ConnectionMongo.php';
   
 $coll = $db->Store_Summary_Visit_Point;
 
-$result = $coll->find();
+$result = $coll->find(array("Start_Date" => array('$gt' => $start, '$lte' => $end)));
 
 $arrayResult = Array();
   
