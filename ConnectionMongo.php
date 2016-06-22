d7b3ab0905d306538df207dab6b91c8ed922c974
Katherine Fallas <Katherinefallasc@gmail.com>
2016-06-22 20:49:45 +0000

    Development

--- ConnectionMongo.php
+++ ConnectionMongo.php
@@ -1,7 +1,7 @@
 <?php
 try{
 
-  
+echo "Development";  
 //$uri = "mongodb://Eprac_Testing:eprac123@ds047812.mlab.com:47812/eflowdts_testing"; 
 //$uri = "mongodb://Eprac_Production:eprac123@ds031223.mlab.com:31223/eflowdts_production"; 
 $uri = "mongodb://Eprac_Development:eprac123@ds053658.mlab.com:53658/eflowdts_development"; 
