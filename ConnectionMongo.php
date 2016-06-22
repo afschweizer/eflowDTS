71f7cd43315d39e439fc2da7be3aa54576a14de2
Katherine Fallas <Katherinefallasc@gmail.com>
2016-06-22 21:06:18 +0000

    production connection

--- ConnectionMongo.php
+++ ConnectionMongo.php
@@ -2,16 +2,16 @@
 try{
 
 //$uri = "mongodb://Eprac_Testing:eprac123@ds047812.mlab.com:47812/eflowdts_testing"; 
-//$uri = "mongodb://Eprac_Production:eprac123@ds031223.mlab.com:31223/eflowdts_production"; 
-$uri = "mongodb://Eprac_Development:eprac123@ds053658.mlab.com:53658/eflowdts_development"; 
+$uri = "mongodb://Eprac_Production:eprac123@ds031223.mlab.com:31223/eflowdts_production"; 
+//$uri = "mongodb://Eprac_Development:eprac123@ds053658.mlab.com:53658/eflowdts_development"; 
 
 $options = array("connectTimeoutMS" => 30000);
 
 $conn = new MongoClient($uri,$options);
   
 //$db = $conn->selectDB("eflowdts_testing");
-//$db = $conn->selectDB("eflowdts_production");
-$db = $conn->selectDB("eflowdts_development");
+$db = $conn->selectDB("eflowdts_production");
+//$db = $conn->selectDB("eflowdts_development");
   
   
 }
