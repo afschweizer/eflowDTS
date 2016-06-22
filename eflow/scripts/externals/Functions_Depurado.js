3f83c8af93e44f360698d7cee45ae94966f101fc
Katherine Fallas <Katherinefallasc@gmail.com>
2016-06-22 20:55:34 +0000

    development

--- eflow/scripts/externals/Functions_Depurado.js
+++ eflow/scripts/externals/Functions_Depurado.js
@@ -7,7 +7,7 @@
 		},
     Configuration: {
         "URLs": { 
-            "eflow_Get": "http://104.197.6.251/eflowDTS_Testing/eflowDTS/Eflow_Get.php",
+            "eflow_Get": "http://104.197.6.251/eflowDTS_Development/eflowDTS/Eflow_Get.php",
             "eflow_Post": "http://104.197.6.251/eflowDTS_Development/eflowDTS/Eflow_Post.php",
             "eflow_Date_Time" : "http://104.197.6.251/eflowDTS_Development/eflowDTS/time.php"
         }
