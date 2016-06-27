25cd6df8a01495ac43f549118aa8a0ac289802fb
Katherine Fallas <Katherinefallasc@gmail.com>
2016-06-27 17:13:45 +0000

    eflowDTS_Development

--- eflow/scripts/externals/Functions_Depurado.js
+++ eflow/scripts/externals/Functions_Depurado.js
@@ -7,9 +7,9 @@ google.load("visualization", "1", {packages:["corechart"/*, "charteditor"*/]});
 		},
     Configuration: {
         "URLs": { 
-            "eflow_Get": "http://104.197.6.251/eflowDTS_Testing/eflowDTS/Eflow_Get.php",
-            "eflow_Post": "http://104.197.6.251/eflowDTS_Testing/eflowDTS/Eflow_Post.php",
-            "eflow_Date_Time" : "http://104.197.6.251/eflowDTS_Testing/eflowDTS/time.php"
+            "eflow_Get": "http://104.197.6.251/eflowDTS_Development/eflowDTS/Eflow_Get.php",
+            "eflow_Post": "http://104.197.6.251/eflowDTS_Development/eflowDTS/Eflow_Post.php",
+            "eflow_Date_Time" : "http://104.197.6.251/eflowDTS_Development/eflowDTS/time.php"
         } 
     }, 
     Session: {},
