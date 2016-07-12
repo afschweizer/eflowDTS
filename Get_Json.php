8a1e9ef2852e9871065c3bee03d4888f9db57422
Katherine Fallas <katherinefallasc@gmail.com>
2016-07-12 15:33:21 +0000

    Cambios diferentes Scr

--- Get_Json.php
+++ Get_Json.php
@@ -7,7 +7,7 @@ header("Access-Control-Allow-Origin: * ");
 header("Access-Control-Allow-Credentials: true ");
 header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
 header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
- 
+       
 //Json to Array      
 $json = file_get_contents('php://input');
 $dataObject = json_decode($json);
