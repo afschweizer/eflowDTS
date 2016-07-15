6a917708f8aae6594390645ed5efc2d102d275e2
Katherine Fallas <katherinefallasc@gmail.com>
2016-07-15 16:55:14 +0000

    d

--- Select_All_User.php
+++ Select_All_User.php
@@ -7,7 +7,7 @@ $coll = $db->Store_User_Access;
 $result = $coll->find($dataObject->Data,$dataObject->Fields);
 
 $arrayResult = Array();
-  
+   
 foreach($result as $doc){
  
   array_push($arrayResult, $doc);
