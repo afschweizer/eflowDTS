016ac0e6e8982abca28a931052dc6e9450b3d344
Katherine Fallas <katherinefallasc@gmail.com>
2016-07-19 17:22:45 +0000

    Importar vp

--- Get_Json.php
+++ Get_Json.php
@@ -7,8 +7,6 @@ header("Access-Control-Allow-Origin: * ");
 header("Access-Control-Allow-Credentials: true ");
 header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
 header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
-       
-require('Functions_Get_Json.php');
   
 $Json_String = file_get_contents('php://input'); 
   
@@ -22,8 +20,7 @@ for($i = 0; count($Json_Object); $i++){
     
     $coll->insert($Json_Object[$i]);
     
-}
-  
+}  
   
   
 }catch(Exception $e){
