b1c0bad902c1293cf6a71d368405ee3447c28aeb
Katherine Fallas <katherinefallasc@gmail.com>
2016-07-19 15:27:29 +0000

    importar

--- Get_Json.php
+++ Get_Json.php
@@ -8,6 +8,7 @@ header("Access-Control-Allow-Credentials: true ");
 header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
 header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
        
+require('Functions_Get_Json.php');
   
 $Json_String = file_get_contents('php://input'); 
   
