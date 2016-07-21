c59e2964f0870a43b833c2a918ad4c451f5d0e57
Katherine Fallas <katherinefallasc@gmail.com>
2016-07-21 20:28:51 +0000

    All change

--- Get_Json.php
+++ Get_Json.php
@@ -17,13 +17,25 @@ include 'ConnectionMongo.php';
 $coll = $db->Store_Jobs_Test;
 
 for($i = 0; $i < count($Json_Object); $i++){
-    
-    $coll->insert($Json_Object[$i]);
+  
+    $doc = $Json_Object[$i];
+  		
+   
+		//$doc->Control.Creation_Date = date_timestamp_get(date_create())*1000;
+		//$doc->Control.Created_User = "Rest_Service";		
+		$doc->Manager = "Cualquiera";		
+		$doc->Visit_State = "Unassigned";		
+		//$doc->Collection_Info.Collection_Name = "Store_Jobs";
+      
+  
+  
+    $coll->insert($doc);
      
 }  
   
-//echo json_encode(array('Message'=> 'Insertados '.count($Json_Object). ' documentos.'));
-  echo $Json_String;
+  
+echo json_encode(array('Message'=> 'Insertados '.count($Json_Object). ' documentos.'));
+  
 }catch(Exception $e){
   
   echo json_encode( array('Error' => $e->getMessage()));
