22278933a009cc42d6d94b2a354d5ffa8117f447
Katherine Fallas <katherinefallasc@gmail.com>
2016-08-01 19:39:05 +0000

    cambios totales

---
+++ Insert.php
@@ -0,0 +1,36 @@
+<?php
+header('Access-Control-Allow-Origin: * ');
+header('Access-Control-Allow-Credentials: true ');
+header('Access-Control-Allow-Methods: OPTIONS, GET, POST');
+header('Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control');
+ 
+//Json to Array      
+$json = file_get_contents('php://input');
+$dataObject = json_decode($json);
+
+include 'ConnectionMongo.php'; 
+  
+$coll = $db-> Store_Company;
+
+$Array_Company_Length = count($dataObject->Company_Data);
+
+for($i = 0; $i < $Array_Company_Length; $i++){
+
+  $result = $coll->insert($dataObject->Company_Data[$i]);
+
+}
+echo json_encode(array("Message"=>"Insertado"));
+
+ 
+$coll1 = $db-> Store_User_Access ;
+
+$Array_User_Length = count($dataObject->User_Data);
+
+for($i = 0; $i < $Array_User_Length; $i++){
+
+  $result = $coll1->insert($dataObject->User_Data[$i]);
+
+}
+echo json_encode(array("Message"=>"Insertado"));
+
+?>
