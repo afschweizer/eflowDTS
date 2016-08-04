ae4d4ae3ac06899901d889468fe0d121958212a9
Yeison Fallas <Yeisonfallas@gmail.com>
2016-08-04 15:15:18 +0000

    confirmación de sincronización por medios de tokens

---
+++ Confirm _Synch.php
@@ -0,0 +1,48 @@
+<?php
+
+include 'ConnectionMongo.php'; 
+  
+$coll = $db->Store_Jobs_Send;
+
+//$coll = $db->Store_Jobs_Send;
+ 
+$result = $coll->find($dataObject->Data);
+
+$arrayResult = Array();
+
+foreach($result as $doc){
+ 
+array_push($arrayResult, $doc);
+  
+$coll->update(array("_id"=> new MongoId($doc['_id'])), array('$set' => array("Transferring_State" => "Sync_With_Mobile","Control.Syncroniced_Date"=>date_timestamp_get(date_create())*1000)));
+
+}
+
+$coll = $db->Store_Notification;
+
+$result = $coll->find($dataObject->Data);
+
+foreach($result as $doc){
+  
+array_push($arrayResult, $doc);
+  
+$coll->update(array("_id"=> new MongoId($doc['_id'])), array('$set' => array("Transferring_State"  => "Sync_With_Mobile","Control.Syncroniced_Date"=>date_timestamp_get(date_create())*1000)));
+  
+}
+
+
+/*$coll = $db->Store_Vehicle;
+
+$result = $coll->find($dataObject->Data->ID_Truck);
+
+foreach($result as $doc){
+ 
+ array_push($arrayResult, $doc);
+ //$coll->update(array("_id"=> new MongoId($doc['_id'])), array('$set' => array("Transferring_State"  => "Sync_With_Mobile","Control.Syncroniced_Date"=>date_timestamp_get(date_create())*1000)));
+
+}
+*/
+
+echo json_encode(array("Result"=>"OK"));
+
+?>
