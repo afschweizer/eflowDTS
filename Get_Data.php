01a812f9b0298193f6e1ccc9e87c3e99e8a8f0e8
Yeison Fallas <Yeisonfallas@gmail.com>
2016-08-04 15:07:54 +0000

    Creación de Tokens para la sincronización

--- Get_Data.php
+++ Get_Data.php
@@ -1,6 +1,21 @@
 <?php
 
 include 'ConnectionMongo.php'; 
+
+
+function getGUID(){
+        mt_srand((double)microtime()*10000);
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);// "-"
+        $uuid = substr($charid, 0, 8).$hyphen
+               .substr($charid, 8, 4).$hyphen
+               .substr($charid,12, 4).$hyphen
+               .substr($charid,16, 4).$hyphen
+               .substr($charid,20,12);
+        return $uuid; 
+}
+
+$GUID = getGUID();
   
 $coll = $db->Store_Jobs_Send;
 
@@ -14,7 +29,7 @@ foreach($result as $doc){
  
  array_push($arrayResult, $doc);
  //$coll->update(array("_id"=> new MongoId($doc['_id'])), array('$set' => array("Transferring_State" => "Sync_With_Mobile","Control.Syncroniced_Date"=>date_timestamp_get(date_create())*1000)));
- $coll->update(array("_id"=> new MongoId($doc['_id'])), array('$set' => array("Token_Sincronize" => "Sync_With_Mobile","Control.Syncroniced_Date"=>date_timestamp_get(date_create())*1000)));
+ $coll->update(array("_id"=> new MongoId($doc['_id'])), array('$set' => array("Token_Synch" => $GUID));
 
 }
 
@@ -26,12 +41,12 @@ foreach($result as $doc){
   
  array_push($arrayResult, $doc);
  //$coll->update(array("_id"=> new MongoId($doc['_id'])), array('$set' => array("Transferring_State"  => "Sync_With_Mobile","Control.Syncroniced_Date"=>date_timestamp_get(date_create())*1000)));
- $coll->update(array("_id"=> new MongoId($doc['_id'])), array('$set' => array("Transferring_State"  => "Sync_With_Mobile","Control.Syncroniced_Date"=>date_timestamp_get(date_create())*1000)));
+ $coll->update(array("_id"=> new MongoId($doc['_id'])), array('$set' => array("Token_Synch" => $GUID));
   
 }
 
 
-$coll = $db->Store_Vehicle;
+/*$coll = $db->Store_Vehicle;
 
 $result = $coll->find($dataObject->Data->ID_Truck);
 
@@ -41,8 +56,8 @@ foreach($result as $doc){
  //$coll->update(array("_id"=> new MongoId($doc['_id'])), array('$set' => array("Transferring_State"  => "Sync_With_Mobile","Control.Syncroniced_Date"=>date_timestamp_get(date_create())*1000)));
 
 }
+*/
 
-
-echo json_encode($arrayResult);
+echo json_encode(array("Data"=>$arrayResult,"Token_Synch"=>$GUID);
 
 ?>
