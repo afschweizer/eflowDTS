84cd4b1fb72d9ebc9b53b43ee01ffd0036454f72
Katherine Fallas <katherinefallasc@gmail.com>
2016-09-13 20:42:29 +0000

    q

--- eflow/scripts/controllers/Scr_General_Detail_Controller.js
+++ eflow/scripts/controllers/Scr_General_Detail_Controller.js
@@ -3,6 +3,100 @@ DTS_APP.controller('Scr_General_Detail_Controller',function($scope){
 $scope.init = function(){
 	$scope.Show_Route=false;
  };
+ 
+ 
+ //Gra. 1
+ /* Metodos a utilizar:
+ Select_Jobs
+ Select_Vehicle
+ 
+ */
+ function Get_Total_Weight_Jobs(Arr){
+ 	var Total_Weight = 0;
+ 	for(var i = 0; i<Arr.length;i++){
+ 		Total_Weight += Arr[i].JobWeight;
+ 	}
+ 	return Total_Weight;
+ }
+
+ function Get_Total_Weight_Truck(Arr){
+ 	var Total_Weight = 0;
+ 	for(var i = 0; i < Arr.length; i++){
+ 		Total_Weight += Arr[i].Weight;
+ 	}
+ 	return Total_Weight;
+ }
+
+//Gra. 2
+/* Metodos a utilizar:
+Select_Jobs
+
+*/
+function Get_Total_Order_Damaged(Orders){
+	var Total_Order = 0;
+	for(var i =0; i < Orders.length;i++){
+		var Damaged = false;
+		var Jobs = Orders[i].Jobs;
+		for(var j = 0; j < Jobs.length;j++){
+			var Actions = Jobs[j].JobActions;
+			for(var x = 0; x < Actions.length;x++){
+				if(Actions[x].Action_Type === "Mal_Estado"){
+					Damaged = true;
+					break;
+				}
+			}		
+		}
+		if(Damaged === true){
+			Total_Order++;
+		}
+	}
+	return Total_Order;
+}
+
+//Gra. 3
+
+function Get_(){}
+
+//Gra. 8
+/* Metodos a utilizar:
+Select_Summary_Trip
+
+*/
+function Get_On_Time_Delivery(trip){
+	var Total_Order_Time = 0;
+	var TotalJobs = 0;
+	var TotalTime = 0;
+	for(var i =0; i < trip.length;i++){
+	 TotalJobs = TotalJobs + trip[i].Total_Jobs ;
+	 TotalTime = TotalTime + trip[i].Total_Doc_In_Time ;
+	}
+	Total_Order_Time =(TotalTime/TotalJobs);
+	return Total_Order_Time;
+}
+//Gra. 9
+/* Metodos a utilizar:
+Select_Summary_Trip
+
+*/
+function Get_Efficiency_Delivery(trip){
+	var Total_Efficiency_Delivery = 0;
+	var TotalUnits = 0;
+	var TotalComplete = 0;
+	for(var j =0; j < trip.length;j++){
+	 TotalUnits = TotalUnits + trip[j].Total_Units ;
+	 TotalComplete = TotalComplete + trip[j].Total_Complete_Units ;
+	}
+	Total_Efficiency_Delivery =(TotalComplete/TotalUnits);
+	return Total_Efficiency_Delivery;
+}
 
 
+//Gra.7
+/* Metodos a utilizar:
+Select_Summary_Trip
+
+*/
+function Get_Drop_Size(){
+	
+}
 });
