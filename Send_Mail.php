1dadcf87630f0103f7b10cb7f53b9219a8297dbb
Yeison Fallas <Yeisonfallas@gmail.com>
2016-10-27 22:18:29 +0000

    drfxcffgh

--- Send_Mail.php
+++ Send_Mail.php
@@ -1,41 +1,44 @@
 <?php
-
-date_default_timezone_set('Etc/UTC');
-
 require 'Mail/PHPMailerAutoload.php';
 
-$mail = new PHPMailer;
+include 'ConnectionMongo.php';
 
-$mail->isSMTP();
+$Coll_Mails = $db->Store_Mail_Status;
+$Coll_Jobs = $db->Store_Jobs_Send;
+$List_Mails = $Coll_Mails->find(array("Status" => "PEND"));
 
-$mail->SMTPDebug = 0;
+foreach($List_Mails as $doc){
+	
+	$PDF = $Coll_Jobs->findOne(array('_id' => new MongoId($doc->ID_PDF)),array("Certificate" => true,"Name" => true));
+	
+	$String_PDF = str_replace("data:application/pdf;base64,", "", $PDF->Certificate->PDF);
+	$mail = new PHPMailer;
+	$mail->isSMTP();
+	$mail->SMTPDebug = 0;
+	$mail->Debugoutput = 'html';
+	$mail->Host = "smtpout.secureserver.net";
+	$mail->Port = 25;
+	$mail->SMTPAuth = true;
+	$mail->Username = "info@eprac.com";
+	$mail->Password = "wolFe224";
+	$mail->setFrom('DTS_TRACK@eprac.com', 'DTS Track');
+	// $mail->addCC('yeison.fallas@eprac.com', 'Katherine Fallas');
+	foreach($doc->Mails as $Contact){
+		$mail->addAddress($Contact->Value, $Contact->Description);
+	}
+	$mail->Subject = $PDF->Name . ' ' . $doc->PDF_Date;
+	$mail->Body = "PDF";
+	// $mail->AltBody = 'This is a plain-text message body';
+	$pdf_decoded = base64_decode($String_PDF);
+	$mail->AddStringAttachment($pdf_decoded, 'BASE64.pdf');
+	if (!$mail->send()){
+		$List_Mails->update(array("_id"=> new MongoId($doc['_id'])), array('$set' => array("Status"  => "ERROR","Error_Message"  => $mail->ErrorInfo;)));
+	}else{
+		$List_Mails->update(array("_id"=> new MongoId($doc['_id'])), array('$set' => array("Status"  => "SEND","PDF_Date_Send"  => date("Y-m-d"))));
+	}
+	
+	}
 
-$mail->Debugoutput = 'html';
+echo "Listo!";
 
-$mail->Host = "smtpout.secureserver.net";
-
-$mail->Port = 25;
-
-$mail->SMTPAuth = true;
-
-$mail->Username = "info@eprac.com";
-
-$mail->Password = "wolFe224";
-
-$mail->setFrom('DTS_TRACK@eprac.com', 'DTS Track');
-
-//$mail->addReplyTo('katherine.fallas@eprac.com', 'Katherine Fallas');
-
-$mail->addAddress('yeisonfallas@gmail.com', 'Yeison Fallas');
-
-$mail->Subject = 'Testing From APP Engine';
-
-$mail->Body = "Hola Mundo!.";
-
-$mail->AltBody = 'This is a plain-text message body';
-
-if (!$mail->send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-    echo "Message sent!";
-}
+?>
